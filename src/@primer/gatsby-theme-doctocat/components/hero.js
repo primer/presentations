ced5f72094e5a363a8b7530ad85833809f69de7a
Ash Guillaume <10384315+ashygee@users.noreply.github.com>
2020-12-11 13:50:13 -0800

    update with Yaili's suggestions

--- src/@primer/gatsby-theme-doctocat/components/hero.js
+++ src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -13,7 +13,7 @@ export default function Hero() {
         <Text as="div" color="blue.2" fontSize={2} mb={4}>
           Last updated: March 22, 2019
         </Text>
-        <img src={heroImage} alt="Primer Presentations hero" />
+        <img src={heroImage} alt="Primer Presentations hero image" />
       </Container>
     </Box>
   )

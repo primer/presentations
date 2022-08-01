85f1ae3709425743b51a05c4b8e1cbd334cf5f01
Josep Martins <josepmartins@github.com>
2022-08-01 17:35:20 +0200

    Add @primer/react, upgrade Hero component

--- src/@primer/gatsby-theme-doctocat/components/hero.js
+++ src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -1,20 +1,19 @@
-import {Box, Heading, Text} from '@primer/components'
+import {Box, Heading, ThemeProvider} from '@primer/react'
 import React from 'react'
 import heroImage from '../../../hero-image.svg'
 import {Container} from '@primer/gatsby-theme-doctocat'
 
 export default function Hero() {
   return (
-    <Box bg="black" p={5}>
-      <Container>
-        <Heading color="blue.4" fontSize={7} pb={3} m={0}>
-          Primer Presentations
-        </Heading>
-        <Text as="div" color="blue.2" fontSize={2} mb={4}>
-          Last updated: March 22, 2019
-        </Text>
-        <img src={heroImage} alt="Primer Presentations hero image" />
-      </Container>
-    </Box>
+    <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+      <Box bg="canvas.default" py={6}>
+        <Container>
+          <Heading sx={{color: 'accent.fg', fontSize: 7, lineHeight: 'condensed', pb: 3, m: 0}}>
+            Primer Presentations
+          </Heading>
+          <img src={heroImage} alt="Interface Guidelines hero" width="100%" />
+        </Container>
+      </Box>
+    </ThemeProvider>
   )
 }

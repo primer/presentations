9a4c4756fed9cca5f784e1647d19330549409868
Josep Martins <josepmartins@github.com>
2022-08-02 14:08:51 +0200

    Bump @primer/gatsby-theme-doctocat from 3.1.1 to 4.0.0

--- src/@primer/gatsby-theme-doctocat/components/hero.js
+++ src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -1,20 +1,22 @@
-import {Box, Heading, Text} from '@primer/components'
+import {Box, Heading, ThemeProvider, Text} from '@primer/react'
 import React from 'react'
-import heroImage from '../../../hero-image.svg'
 import {Container} from '@primer/gatsby-theme-doctocat'
+import heroImage from '../../../hero-image.svg'
 
 export default function Hero() {
   return (
-    <Box bg="black" p={5}>
-      <Container>
-        <Heading color="blue.4" fontSize={7} pb={3} m={0}>
-          Primer Presentations
-        </Heading>
-        <Text as="div" color="blue.2" fontSize={2} mb={4}>
-          Last updated: March 22, 2019
-        </Text>
-        <img src={heroImage} alt="Primer Presentations hero image" />
-      </Container>
-    </Box>
+    <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+      <Box bg="canvas.default" py={6}>
+        <Container>
+          <Heading as="h1" sx={{color: 'accent.fg', fontSize: 7, m: 0}}>
+            Primer Presentations
+          </Heading>
+          <Text as="p" sx={{mt: 0, mb: 4, color: 'fg.default', fontSize: 4}}>
+            Presentation system for Primer
+          </Text>
+          <img src={heroImage} alt="Interface Guidelines hero" width="100%" />
+        </Container>
+      </Box>
+    </ThemeProvider>
   )
 }
